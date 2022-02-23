28884310ccea7412533fae317ef0da069009e5e3
Sheff1889 <sebastian1889.r@gmx.at>
2022-02-23 16:52:34 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const navSlide = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.querySelector(".nav-links");
+
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+
+        //Hamburger animation 
+
+        hamburger.classList.toggle('toggle'); 
+        
+
+    });
+}
+
+navSlide();
